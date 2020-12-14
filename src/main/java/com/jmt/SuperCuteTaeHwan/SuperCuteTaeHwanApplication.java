8e041c2142c6b9edd9c2d68ce8cec8edde0e12b8
Han Kyoung Min <48062593+Malang2@users.noreply.github.com>
2020-12-14 22:04:02 +0900

    Initial commit

---
+++ src/main/java/com/jmt/SuperCuteTaeHwan/SuperCuteTaeHwanApplication.java
@@ -0,0 +1,13 @@
+package com.jmt.SuperCuteTaeHwan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperCuteTaeHwanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperCuteTaeHwanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jmt/SuperCuteTaeHwan/SuperCuteTaeHwanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmt.SuperCuteTaeHwan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperCuteTaeHwanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
